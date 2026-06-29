dc72b71fd20437bf04454d0cb7bd42c558010ed6
Dev-BizDateUp <support@bizdateup.com>
2026-06-29 10:15:31 +0530

    fix: correct typos in Learning.txt and remove unused services from backend

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,9 @@
 require("dotenv").config();
-const {
-  resmune,
-  jobDescription,
-  selfDescription,
-} = require("./src/services/temp.js");
+
 const app = require("./src/app.js");
 const connectDb = require("./src/config/database.js");
-const generateInterviewReportOpenRouter = require("./src/services/openrouter.service.js");
 const generateInterviewReportGroq = require("./src/services/groq.service.js");
-const generateInterviewReportGemini = require("./src/services/gemini.service.js");
-// generateInterviewReportGemini({
-//   resmune,   
-//   jobDescription,
-//   selfDescription,
-// });
-
-// generateInterviewReportOpenRouter({
-//   resmune,
-//   jobDescription,
-//   selfDescription,
-// });
-// generateInterviewReportGroq({
-//   resmune, 
-//   jobDescription,
-//   selfDescription,
-// });
 connectDb();
-app.listen(process.env.PORT, () => {});
+app.listen(process.env.PORT, () => {
+  console.log("App Connected Successfully")
+});
